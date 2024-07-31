347bfe180632597d9e3d1e7ec06624f44a65064a
anhuong541 <nguyenxuananhuong541@gmail.com>
2024-07-31 10:36:28 +0900

    set up some thing

---
+++ src/app/project/[id]/[name-project]/page.tsx
@@ -0,0 +1,10 @@
+export default function ProjectDetailPage() {
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        <p>hello world</p>
+        <>asfhibasifbiasb</>
+      </div>
+    </>
+  );
+}
