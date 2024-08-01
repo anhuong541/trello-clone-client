17df6269feefa79e3ac4bdefb3888800651c00ab
anhuong541 <nguyenxuananhuong541@gmail.com>
2024-08-01 09:38:53 +0700

    perf: add try catch into firebaseAction

--- src/lib/firebaseActions.ts
+++ src/lib/firebaseActions.ts
@@ -1,13 +1,30 @@
+import { auth } from "./firebase";
 import {
+  signOut,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-} from "@firebase/auth";
-import { auth } from "./firebase";
+} from "firebase/auth";
 
 export const handleUserRegister = async (email: string, password: string) => {
-  await createUserWithEmailAndPassword(auth, email, password);
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log("register firebase error: ", error);
+  }
 };
 
 export const handleUserSignIn = async (email: string, password: string) => {
-  await signInWithEmailAndPassword(auth, email, password);
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log("login firebase error: ", error);
+  }
+};
+
+export const handleUserSignOut = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.log("sign out firebase error: ", error);
+  }
 };
