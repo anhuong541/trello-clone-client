15d3e934832e79f519a08dc824e93f8603314ad0
anhuong541 <nguyenxuananhuong541@gmail.com>
2024-08-01 10:04:56 +0700

    chore: add redirect to unused page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
 "use client";
 
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <main className="flex gap-4 p-4">
-      <Link
-        href="/login"
-        className="px-4 p-2 bg-green-600 text-white rounded-md font-medium"
-      >
-        Login
-      </Link>
-      <Link
-        href="/register"
-        className="px-4 p-2 bg-green-600 text-white rounded-md font-medium"
-      >
-        Register
-      </Link>
-    </main>
-  );
+export default async function Home() {
+  redirect("/login");
 }
