1b6d5257778af74304e794a67e8fe0747942f21e
anhuong541 <nguyenxuananhuong541@gmail.com>
2024-08-07 16:40:34 +0700

    chore: comment unused code on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,16 +8,16 @@ const routesBanWhenUserSignin = ["/login", "/register", ROOT_ROUTE];
 export async function middleware(request: NextRequest) {
   const session = request.cookies.get(SESSION_COOKIE_NAME)?.value || "";
 
-  // Redirect to login if session is not set
-  if (!session && protectedRoutes.includes(request.nextUrl.pathname)) {
-    const absoluteURL = new URL("/login", request.nextUrl.origin);
-    return NextResponse.redirect(absoluteURL.toString());
-  }
+  // // Redirect to login if session is not set
+  // if (!session && protectedRoutes.includes(request.nextUrl.pathname)) {
+  //   const absoluteURL = new URL("/login", request.nextUrl.origin);
+  //   return NextResponse.redirect(absoluteURL.toString());
+  // }
 
-  if (session && routesBanWhenUserSignin.includes(request.nextUrl.pathname)) {
-    const absoluteURL = new URL(HOME_ROUTE, request.nextUrl.origin);
-    return NextResponse.redirect(absoluteURL.toString());
-  }
+  // if (session && routesBanWhenUserSignin.includes(request.nextUrl.pathname)) {
+  //   const absoluteURL = new URL(HOME_ROUTE, request.nextUrl.origin);
+  //   return NextResponse.redirect(absoluteURL.toString());
+  // }
 }
 
 // See "Matching Paths" below to learn more
