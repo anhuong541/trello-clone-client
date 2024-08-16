ad4a2c30e2ed1b5c0d7d4955ea762ae7d3c4d412
anhuong541 <nguyenxuananhuong541@gmail.com>
2024-08-16 15:06:52 +0700

    update server domain

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const server = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_SERVER_DOMAIN ?? "http://localhost:3456/",
-  baseURL: "http://localhost:3456/",
+  baseURL: process.env.NEXT_PUBLIC_SERVER_DOMAIN ?? "http://localhost:3456/",
+  // baseURL: "http://localhost:3456/",
   withCredentials: true,
 });
