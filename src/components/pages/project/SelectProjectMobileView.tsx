714ad1deb19ae2e2a7c40184b1c9d5917c10ded5
anhuong541 <nguyenxuananhuong541@gmail.com>
2024-08-26 13:54:16 +0700

    add list project at mobile view

---
+++ src/components/pages/project/SelectProjectMobileView.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { handleUserProjectList } from "@/actions/query-actions";
+import { ProjectListItem } from "@/components/layouts/Sidebar/Sidebar";
+import { reactQueryKeys } from "@/lib/react-query-keys";
+import { Text } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
+import { useMemo } from "react";
+
+export default function SelectProjectMobileView() {
+  const queryUserProjectList = useQuery({
+    queryKey: [reactQueryKeys.projectList],
+    queryFn: handleUserProjectList,
+  });
+
+  const userProjectList: ProjectListItem[] = useMemo(() => {
+    const data: ProjectListItem[] =
+      (queryUserProjectList && queryUserProjectList.data?.data.data) ?? [];
+
+    const formatDataUserProjectList = data.sort((a, b) => b?.dueTime - a?.dueTime);
+
+    return formatDataUserProjectList;
+  }, [queryUserProjectList]);
+
+  console.log({ userProjectList });
+
+  return (
+    <div className="col-span-8 bg-blue-100 pt-4 h-full lg:hidden">
+      <div className="flex flex-col">
+        <Text className="px-4" fontWeight={700}>
+          Your projects
+        </Text>
+        <div className="grid sm:grid-cols-2 grid-cols-1 gap-4 px-4 py-2">
+          {userProjectList.map((item) => {
+            return (
+              <Link
+                href={`/project/${item.projectId}`}
+                key={item.projectId}
+                className="bg-white flex flex-col gap-2 p-4 rounded-md active:bg-blue-50"
+              >
+                <Text fontSize="lg" fontWeight={700}>
+                  {item.projectName}
+                </Text>
+                <Text color="gray">{item.description !== "" ? item.description : "..."}</Text>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/pages/project/index.tsx
+++ src/components/pages/project/index.tsx
@@ -1,13 +1,18 @@
 import KanbanBoard from "@/components/pages/project/kanbanTable/KanbanBoard";
 import { Sidebar } from "@/components/layouts/Sidebar";
+import SelectProjectMobileView from "./SelectProjectMobileView";
 
 export default function Project({ projectId }: { projectId: string }) {
   if (projectId === "") {
     return (
-      <div className="lg:grid lg:grid-cols-10 flex h-full">
-        <Sidebar projectId="" />
-        <div className="col-span-8 bg-blue-100" />
-      </div>
+      <>
+        <div className="lg:grid lg:grid-cols-10 hidden h-full">
+          <Sidebar projectId="" />
+          <div className="col-span-8 bg-blue-100" />
+        </div>
+
+        <SelectProjectMobileView />
+      </>
     );
   }
 
