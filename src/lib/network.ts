4746f954e696d5bab627885430dcc30d424a1503
anhuong541 <nguyenxuananhuong541@gmail.com>
2024-08-29 17:32:08 +0700

    update deploy logic

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const isProduction = process.env.NEXT_PUBLIC_NODE_ENV === "production";
+
 export const server = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_DOMAIN,
+  baseURL: isProduction ? process.env.NEXT_PUBLIC_SERVER_DOMAIN : "http://localhost:3456/",
   withCredentials: true,
 });
