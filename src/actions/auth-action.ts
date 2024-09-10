fb3bfc91337ba206d378bb58e7e28bd50bcbbccb
huong541 <nguyenxuananhuong541@gmail.com>
2024-09-10 21:20:44 +0700

    update auth

--- src/actions/auth-action.ts
+++ src/actions/auth-action.ts
@@ -6,7 +6,7 @@ import { AUTH_TOKEN, HOME_ROUTE, ROOT_ROUTE, SESSION_COOKIE_NAME } from "@/const
 import { isProduction } from "@/lib/network";
 
 export async function createSession(authToken: string) {
-  cookies().set(SESSION_COOKIE_NAME, authToken, {
+  await cookies().set(SESSION_COOKIE_NAME, authToken, {
     httpOnly: true,
     // secure: process.env.NODE_ENV === "production",
     maxAge: 60 * 60 * 24, // One day
@@ -14,16 +14,17 @@ export async function createSession(authToken: string) {
   });
 
   if (isProduction) {
-    cookies().set(AUTH_TOKEN, authToken, {
+    await cookies().set(AUTH_TOKEN, authToken, {
       maxAge: 60 * 60 * 24, // One day
       path: "/",
     });
   }
   // redirect to home route
-  redirect(HOME_ROUTE);
+  await redirect(HOME_ROUTE);
 }
 
 export async function removeSession() {
-  cookies().delete(SESSION_COOKIE_NAME);
-  redirect(ROOT_ROUTE);
+  await cookies().delete(SESSION_COOKIE_NAME);
+  await cookies().delete(AUTH_TOKEN);
+  await redirect(ROOT_ROUTE);
 }
