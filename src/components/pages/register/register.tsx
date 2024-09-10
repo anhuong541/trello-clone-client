c20a4d170d4438859ef83ff5215156b6a0d1cc56
huong541 <nguyenxuananhuong541@gmail.com>
2024-09-10 21:09:42 +0700

    save token to cookie

--- src/components/pages/register/register.tsx
+++ src/components/pages/register/register.tsx
@@ -61,7 +61,6 @@ export default function Register() {
 
     if (res?.status === 200) {
       isProduction && createSession(res?.data.token);
-      isProduction && localStorage.setItem("token", res?.data.token);
       router.push("/active");
       reset();
     } else {
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export const ROOT_ROUTE = "/login";
 export const HOME_ROUTE = "/project";
 export const SESSION_COOKIE_NAME = "user_session";
+export const AUTH_TOKEN = "auth_token";
