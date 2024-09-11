43e9ec7baaa70d9fde8802452d52b23457a700f6
Huong Nguyen <huong.nxa@mor.com.vn>
2024-09-11 09:59:58 +0700

    add redirect facebook callback

--- src/constants/facebook.ts
+++ src/constants/facebook.ts
@@ -1,3 +1,4 @@
 export const FB_APP_ID = process.env.NEXT_PUBLIC_FACEBOOK_APP_ID!; // Replace with your Facebook App ID
-export const REDIRECT_URI = "http://localhost:3000/demo"; // Your callback URL
+export const REDIRECT_URI =
+  "https://trello-clone-client-emq1xg37r-huong541-projects.vercel.app/demo-Facebook"; // Your callback URL
 export const FB_APP_SECRET = process.env.NEXT_PUBLIC_FACEBOOK_APP_SECRET!;
