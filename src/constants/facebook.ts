c61f0beab2dedf702cc367a2cfa823492d538441
Huong Nguyen <huong.nxa@mor.com.vn>
2024-09-11 14:14:41 +0700

    fix deploy for build

--- src/constants/facebook.ts
+++
@@ -1,4 +0,0 @@
-export const FB_APP_ID = process.env.NEXT_PUBLIC_FACEBOOK_APP_ID!; // Replace with your Facebook App ID
-export const REDIRECT_URI =
-  "https://trello-clone-client-emq1xg37r-huong541-projects.vercel.app/demo-Facebook"; // Your callback URL
-export const FB_APP_SECRET = process.env.NEXT_PUBLIC_FACEBOOK_APP_SECRET!;
